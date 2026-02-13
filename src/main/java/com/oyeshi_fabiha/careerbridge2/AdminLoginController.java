2469bd3b24599a46a0f4b127dd3f782273a686f2
oyeshi13 <oyeshi1308@email.com>
2026-02-14 00:08:24 +0600

    added temp homepage

--- src/main/java/com/oyeshi_fabiha/careerbridge2/AdminLoginController.java
+++ src/main/java/com/oyeshi_fabiha/careerbridge2/AdminLoginController.java
@@ -1,9 +1,14 @@
 package com.oyeshi_fabiha.careerbridge2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminLoginController {
 
@@ -17,6 +22,14 @@ public class AdminLoginController {
 
         if (user.equals("admin") && pass.equals("1234")) {
             System.out.println("Login Successful!");
+            try{
+                FXMLLoader new_loader = new FXMLLoader(getClass().getResource("home.fxml"));
+                Scene scene = new Scene(new_loader.load());
+                Stage currStage = (Stage) usernameField.getScene().getWindow();
+                currStage.setScene(scene);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         } else {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setContentText("Invalid Credentials");
